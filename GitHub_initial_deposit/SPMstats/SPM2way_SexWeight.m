034ba3333c07f9e379bc420165be601a2999152f
Jason Bouffard <jason.bouffard@mail.mcgill.ca>
2017-07-17 14:18:13 -0400

    Edit SPM for muscle focus

--- GitHub_initial_deposit/SPMstats/SPM2way_SexWeight.m
+++ GitHub_initial_deposit/SPMstats/SPM2way_SexWeight.m
@@ -1,6 +1,6 @@
-%% SPM: Repeated measure ANOVA 3 way, 2 RM. Y is EMG
-clearvars -except EMG
-Signal='SubScap';
+%% SPM: Repeated measure ANOVA 3 way, 2 RM. Y is MuscleFocus
+clearvars -except MuscleFocus
+Signal='MFBlache';
 
 CondH={'H6H2', 'H12H2'};%, 'H6H2', 'H6H3', 'H6H4', 'H6H5', 'H6H6',...
     %'H12H1', 'H12H2', 'H12H3', 'H12H4', 'H12H5', 'H12H6'};
@@ -9,8 +9,8 @@ CondF={'F6H2', 'F12H2'};% 'F6H2', 'F6H3', 'F6H4', 'F6H5', 'F6H6',...
    % 'F12H1', 'F12H2', 'F12H3', 'F12H4', 'F12H5', 'F12H6'};
 
 for icond =1:length(CondH)
-    dataH(:,:,icond)=squeeze(nanmean(EMG.(Signal).(CondH{icond})(:,:,:),2));
-    dataF(:,:,icond)=squeeze(nanmean(EMG.(Signal).(CondF{icond})(:,:,:),2));
+    dataH(:,:,icond)=squeeze(nanmean(MuscleFocus.(Signal).(CondH{icond})(:,:,:),2));
+    dataF(:,:,icond)=squeeze(nanmean(MuscleFocus.(Signal).(CondF{icond})(:,:,:),2));
 end
 
 sujetH=find(sum(isnan(dataH(1,:,:)),3)==0);
@@ -42,18 +42,18 @@ disp_summ(FFni)
 %(2) Plot:
 close all;
 FFni.plot('plot_threshold_label',true, 'plot_p_values',true, 'autoset_ylim',true);
-FFi        = spm1d.stats.anova2onerm(Y, A, B, SUBJ).inference(alpha);
-%%% compare to parametric results by plotting the parametric thresholds:
-for i = 1:FFi.nEffects
-    Fi = FFi(i);
-    subplot(2,2,i);
-    hold on
-    plot([0 numel(Fi.z)], Fi.zstar*[1 1], 'color','c', 'linestyle','--')
-    hold off
-    text(5, Fi.zstar, 'Parametric', 'color','k', 'backgroundcolor','c')
-end
-
-subplot(2,2,1)
-xlabel(['nH = ', num2str(length(sujetH)), ' nF =', num2str(length(sujetF)), ' Muscle: ', Signal]);
-
-
+% FFi        = spm1d.stats.anova2onerm(Y, A, B, SUBJ).inference(alpha);
+% %%% compare to parametric results by plotting the parametric thresholds:
+% for i = 1:FFi.nEffects
+%     Fi = FFi(i);
+%     subplot(2,2,i);
+%     hold on
+%     plot([0 numel(Fi.z)], Fi.zstar*[1 1], 'color','c', 'linestyle','--')
+%     hold off
+%     text(5, Fi.zstar, 'Parametric', 'color','k', 'backgroundcolor','c')
+% end
+% 
+% subplot(2,2,1)
+% xlabel(['nH = ', num2str(length(sujetH)), ' nF =', num2str(length(sujetF)), ' Muscle: ', Signal]);
+% 
+% 
