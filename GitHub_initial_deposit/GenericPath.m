7f3311ff4a65d4c9e9530baf0b4fa34e300356b6
Unknown <jason.bouffard@mail.mcgill.ca>
2018-01-19 14:09:57 -0500

    Remove machine type from Path

--- GitHub_initial_deposit/GenericPath.m
+++ GitHub_initial_deposit/GenericPath.m
@@ -10,29 +10,11 @@
 
 Path.ServerAddressE=uigetdir('C:','Go get the E: path');
 Path.ServerAddressF=uigetdir('C:','Go get the F: path');
-Path.machinetype=menu('Are you working on a MAC or a PC?', 'PC','MAC');
 
-if Path.machinetype == 1 %If PC Backslashes
-    
-    if isempty(strfind(path, [Path.ServerAddressE, '\Librairies\S2M_Lib\']))
-        % Librairie S2M
-        cd([Path.ServerAddressE '\Librairies\S2M_Lib\']);
-        S2MLibPicker;
-    end
-    
-    
-    %% Setup common paths
-    Path.OpensimSetupJB=[Path.ServerAddressE '\Projet_IRSST_LeverCaisse\Jason\OpenSimSetUpFiles\'];
-    Path.OpensimGenericModel=[Path.OpensimSetupJB,'StandfordVACoRAnatoJB.osim'];
-    Path.OpensimGenericScale=[Path.OpensimSetupJB,'Conf_scaling.xml'];
-    Path.OpensimGenericIK=[Path.OpensimSetupJB,'Conf_IK.xml'];
-    Path.OpensimGenericMD=[Path.OpensimSetupJB,'Conf_MD.xml'];
-    
-    %% Path Elaborated data
-    Path.RBIEMG = [Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\rbiEMG\'];
-    Path.GroupEMG = [Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\MuscleFocus\GroupData\EMG\'];
-    
-elseif Path.machinetype == 2 %If MAC Forward slashes
+Path.ServerAddressE(regexp(Path.ServerAddressE,'\'))='/';
+Path.ServerAddressF(regexp(Path.ServerAddressF,'\'))='/';
+
+
     
     if isempty(strfind(path, [Path.ServerAddressE, '/Librairies/S2M_Lib/']))
         % Librairie S2M
@@ -52,4 +34,3 @@ elseif Path.machinetype == 2 %If MAC Forward slashes
     Path.RBIEMG = [Path.ServerAddressE '/Projet_IRSST_LeverCaisse/ElaboratedData/matrices/rbiEMG/'];
     Path.GroupEMG = [Path.ServerAddressE '/Projet_IRSST_LeverCaisse/ElaboratedData/MuscleFocus/GroupData/EMG/'];
     
-end
--- GitHub_initial_deposit/SubjectPath.m
+++ GitHub_initial_deposit/SubjectPath.m
@@ -1,46 +1,5 @@
 %% Chemin des fichiers
 
-if Path.machinetype == 1 %% If PC (backslashes)
-    
-    % Dossier du sujet
-    Path.DirModels  = [Path.ServerAddressF '\Data\Shoulder\Lib\' Alias.sujet{isujet} 'd\Model_2\'];
-    % Dossier du mod�le pour le sujet
-    Path.pathModel  = [Path.DirModels 'Model.s2mMod'];
-    % Dossier des data
-    Path.importPath = [Path.ServerAddressE '\Projet_IRSST_LeverCaisse\ElaboratedData\matrices\cinematique\' Alias.sujet{isujet} '.mat'];
-    % Dossier static trial
-    Path.importStaticPath = [Path.ServerAddressE '\Projet_Reconstructions\DATA\Romain\' Alias.sujet{isujet} 'd\MODEL2\' Alias.sujet{isujet} 'd1_MOD2.1_rightHanded_GenderH_IRSST_RomMd_.Q2'];
-    % Dossiers d'exportation
-    Path.exportPath = [Path.ServerAddressE '\Projet_IRSST_LeverCaisse\Jason\data\' Alias.sujet{isujet} '\'];
-    
-    if isdir(Path.exportPath)==0
-        mkdir(Path.exportPath);
-    end
-    
-    Path.TRCpath=[Path.exportPath,'TRC\'];
-    if isdir(Path.TRCpath)==0
-        mkdir(Path.TRCpath);
-    end
-    
-    Path.IKpath=[Path.exportPath,'IKOSIM\'];
-    Path.IKresultpath=[Path.IKpath,'result\'];
-    Path.IKsetuppath=[Path.IKpath,'setup\'];
-    if isdir(Path.IKpath)==0
-        mkdir(Path.IKpath);
-        mkdir(Path.IKresultpath);
-        mkdir(Path.IKsetuppath);
-    end
-    
-    Path.MDpath=[Path.exportPath,'MuscleDirection\'];
-    Path.MDresultpath=[Path.MDpath,'result\'];
-    Path.MDsetuppath=[Path.MDpath,'setup\'];
-    if isdir(Path.MDpath)==0
-        mkdir(Path.MDpath);
-        mkdir(Path.MDresultpath);
-        mkdir(Path.MDsetuppath);
-    end
-    
-elseif Path.machinetype == 2 % If MAC forward slashes
     
     % Dossier du sujet
     Path.DirModels  = [Path.ServerAddressF '/Data/Shoulder/Lib/' Alias.sujet{isujet} 'd/Model_2/'];
@@ -80,5 +39,5 @@ elseif Path.machinetype == 2 % If MAC forward slashes
         mkdir(Path.MDsetuppath);
     end
     
-end
+
 
