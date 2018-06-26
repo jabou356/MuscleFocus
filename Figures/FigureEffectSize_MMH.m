d9b49fa2abf4f62f8a0c20d14f9e1a16fed0ef9b
Unknown <jason.bouffard@mail.mcgill.ca>
2018-06-26 13:17:41 -0400

    Effect size figure with posthocs

--- Figures/FigureEffectSize_MMH.m
+++ Figures/FigureEffectSize_MMH.m
@@ -1,10 +1,13 @@
 close all
 clear g
 
+signal = {'DeltA','DeltM','DeltP', 'Lat', 'Pect','Infra', 'Supra','SubScap','BB','TB','MFdelt','MFrotator','MFBlache','DENOMINATORdelt','DENOMINATORrotator','DENOMINATORBlache'};
+
 % Generate figure for SRM with Confidence interval
 % X value: DoF, Y value SRM +- IC, Women Red, Men Blue
 g=gramm('x',ES.variable,'y',ES.cohenD,'size',ES.duration/40);%, 'label', round(data.proportion))
 g.set_color_options('map',[0 0 0])
+g.set_order_options('x',signal)
 
 % Set X Tick Label at 45 deg (names of DoF) and SRM limits between -.2.5
 % and 2.5
@@ -36,8 +39,43 @@ g.set_point_options('base_size',1,'use_input',true,'input_fun',@(s)1+s/5);
 
 g.draw()
 
+g.update('x',ES.variable,'y',ES.cohenD6,'size',ES.durationD6/40);%, 'label', round(data.proportion))
+g.set_color_options('map',[0 1 0])
+g.geom_point()
+%g.facet_grid(ES.effect,[])
+g.set_point_options('base_size',1,'use_input',true,'input_fun',@(s)1+s/5);
 
-g.export('file_name','Figure3_effectsize','file_type','pdf');
+g.draw()
+
+g.update('x',ES.variable,'y',ES.cohenD12,'size',ES.durationD12/40);%, 'label', round(data.proportion))
+g.set_color_options('map',[1 0 1])
+g.geom_point()
+%g.facet_grid(ES.effect,[])
+g.set_point_options('base_size',1,'use_input',true,'input_fun',@(s)1+s/5);
+
+g.draw()
+
+g.update('x',ES.variable,'y',ES.cohenDM,'size',ES.durationDM/40);%, 'label', round(data.proportion))
+g.set_color_options('map',[0 0 1])
+g.geom_point()
+%g.facet_grid(ES.effect,[])
+g.set_point_options('base_size',1,'use_input',true,'input_fun',@(s)1+s/5);
+
+g.draw()
+
+g.update('x',ES.variable,'y',ES.cohenDF,'size',ES.durationDF/40);%, 'label', round(data.proportion))
+g.set_color_options('map',[1 0 0])
+g.geom_point()
+%g.facet_grid(ES.effect,[])
+g.set_point_options('base_size',1,'use_input',true,'input_fun',@(s)1+s/5);
+%g.axe_property('YLim',[0 2]) 
+
+
+g.draw()
+
+
+
+g.export('file_name','Figure_effectsize_posthoc2','file_type','pdf');
 % g.update('x',data.Signal,'y',(data.proportion-50)/100*4,'color',data.Sex)
 % g.set_color_options('map',[0 0 1; 1 0 0])
 % 
